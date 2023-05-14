c8450acb4f914381d18adb342cd5f036d65a6ea7
Lansong <2095621390@qq.com>
2023-05-14 20:34:16 +0800

    完成了es的简单的模糊搜索和精确搜索

--- app/es/cmd/rpc/internal/logic/addTextLogic.go
+++ app/es/cmd/rpc/internal/logic/addTextLogic.go
@@ -6,7 +6,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,25 +25,50 @@ func NewAddTextLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddTextLo
 	}
 }
 
+/*
+参数: 表名(string), 记录(string)
+
+返回: 错误信息(string), 错误error
+*/
 func (l *AddTextLogic) AddText(in *pb.AddTextReq) (*pb.AddTextResp, error) {
-	// 封装添加请求的字段
+	// 序列化
 	data, err := json.Marshal(struct {
 		Context string `json:"context"`
 	}{Context: in.Text})
-
 	if err != nil {
-		return nil, errors.New("封装请求字段失败")
+		return &pb.AddTextResp{
+			Error: fmt.Sprintf("json Marshal error"),
+		}, nil
 	}
+
+	// 构建请求
 	req := esapi.IndexRequest{
 		Index:   in.Index,
 		Body:    bytes.NewReader(data),
 		Refresh: "true",
 	}
+
+	// 发请求
 	res, err := req.Do(context.Background(), l.svcCtx.Es)
 	if err != nil {
-		return nil, errors.New("发送增加请求失败")
+		return &pb.AddTextResp{
+			Error: fmt.Sprintf("do Error"),
+		}, nil
 	}
 	defer res.Body.Close()
+
+	if res.IsError() {
+		return &pb.AddTextResp{
+			Error: fmt.Sprintf("[%s] Error indexing document ID", res.Status()),
+		}, nil
+	} else {
+		var r map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+			return &pb.AddTextResp{
+				Error: fmt.Sprintf("Error parsing the response body: %s", err),
+			}, nil
+		}
+	}
 
 	return &pb.AddTextResp{}, nil
 }
--- app/es/cmd/rpc/internal/logic/searchForLogic.go
+++ app/es/cmd/rpc/internal/logic/searchForLogic.go
@@ -3,7 +3,10 @@ package logic
 import (
 	"CloudMind/app/es/cmd/rpc/internal/svc"
 	"CloudMind/app/es/cmd/rpc/pb"
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -21,7 +24,77 @@ func NewSearchForLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SearchF
 	}
 }
 
-func (l *SearchForLogic) SearchFor(in *pb.SearchForReq) (*pb.SearchForResp, error) {
+/*
+参数： 表名(string), 搜索内容(string), 搜索模式(string)
 
-	return &pb.SearchForResp{}, nil
+返回: 搜索到的数量
+
+tips： 分为精确搜索(match)和模糊搜索(fuzz)
+*/
+func (l *SearchForLogic) SearchFor(in *pb.SearchForReq) (*pb.SearchForResp, error) {
+	var buf bytes.Buffer
+	query := map[string]interface{}{}
+
+	//精确搜索
+	if in.Type == "match" {
+		query = map[string]interface{}{
+			"query": map[string]interface{}{
+				"match": map[string]interface{}{
+					"context": in.Text,
+				},
+			},
+		}
+	} else if in.Type == "fuzz" {
+		// 模糊搜索
+		query = map[string]interface{}{
+			"query": map[string]interface{}{
+				"fuzzy": map[string]interface{}{
+					"context": map[string]interface{}{
+						"value":     in.Text,
+						"fuzziness": "AUTO",
+					},
+				},
+			},
+		}
+	} else {
+		return &pb.SearchForResp{
+			Error: fmt.Sprintf("不支持该搜索方式"),
+		}, nil
+	}
+
+	// 序列化
+	err := json.NewEncoder(&buf).Encode(query)
+	if err != nil {
+		return &pb.SearchForResp{
+			Error: fmt.Sprintf("序列化失败"),
+		}, nil
+	}
+
+	// 搜索
+	res, err := l.svcCtx.Es.Search(
+		l.svcCtx.Es.Search.WithContext(context.Background()),
+		l.svcCtx.Es.Search.WithIndex(in.Index),
+		l.svcCtx.Es.Search.WithBody(&buf),
+		l.svcCtx.Es.Search.WithTrackTotalHits(true),
+		l.svcCtx.Es.Search.WithPretty(),
+	)
+	if err != nil {
+		return &pb.SearchForResp{
+			Error: fmt.Sprintf("search error"),
+		}, nil
+	}
+	defer res.Body.Close()
+	if res.IsError() {
+		return &pb.SearchForResp{
+			Error: fmt.Sprintf("index not found"),
+		}, nil
+	}
+
+	//反序列化搜索结果
+	var r map[string]interface{}
+	_ = json.NewDecoder(res.Body).Decode(&r)
+
+	return &pb.SearchForResp{
+		Number: int64(r["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+	}, nil
 }
